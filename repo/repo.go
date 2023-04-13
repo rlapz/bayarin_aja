5e8a67b936714f4fb7aeb230a872281e3f2d9ad5
Arthur Lapz <rlapz@gnuweeb.org>
2023-04-14 01:38:24 +0700

    repo: Add repo interfaces
    This file contains:
    1. Interface for the `customer`
    2. Interface for the `token`
    3. Interface for the `payment`

---
+++ repo/repo.go
@@ -0,0 +1,20 @@
+package repo
+
+import (
+	"github.com/rlapz/bayarin_aja/model"
+)
+
+type CustomerRepo interface {
+	SelectByUsernme(username string) (*model.Customer, error)
+}
+
+type TokenRepo interface {
+	SelectIdByToken(token string) (int64, error)
+	SelectTokenById(id int64) (string, error)
+	InsertOne(token *model.Token) error
+	DeleteOne(id int64) error
+}
+
+type PaymentRepo interface {
+	InsertOne(payment *model.Payment) error
+}
