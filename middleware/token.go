ae6c239d88e1de4af10fc2a9b16bc8dff87a535d
Arthur Lapz <rlapz@gnuweeb.org>
2023-04-14 02:58:37 +0700

    middleware: token: Add `TokenValidate` function
    Check wheter an action has a valid token or not, reject if not valid

---
+++ middleware/token.go
@@ -0,0 +1,44 @@
+package middleware
+
+import (
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rlapz/bayarin_aja/utils"
+)
+
+/* Desclaimer:
+ *  Don't be scared the use of `goto`
+ *  it can be REALLY useful:
+ *   - Avoid code repetitions
+ *   - Avoid nested-hell
+ *   - .etc
+ */
+func TokenValidate(secret []byte) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var id int64
+		var err error
+
+		authH := ctx.GetHeader("Authorization")
+		if len(authH) == 0 {
+			goto err0
+		}
+
+		if strings.Index(strings.ToLower(authH[:7]), "bearer ") != 0 {
+			goto err0
+		}
+
+		id, err = utils.TokenValidate(authH[7:], secret)
+		if err != nil {
+			goto err0
+		}
+
+		ctx.Set("id", id)
+		ctx.Next()
+		return
+
+	err0:
+		// TODO: add JSON response
+		ctx.Abort()
+	}
+}
