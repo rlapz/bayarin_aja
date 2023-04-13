7e6a78b8fb39c03e8d4136fabf80aca08a31a18b
Arthur Lapz <rlapz@gnuweeb.org>
2023-04-14 01:31:55 +0700

    model: payment.go: Add payment activity model
    This is the `history` of the required interview assignment. (for the payment only)
    I'd like to use the word `activity` instead.

--- model/payment.go
+++ model/payment.go
@@ -12,3 +12,14 @@ type Payment struct {
 	OrderDescription string    `json:"order_description"`
 	CreatedAt        time.Time `json:"created_at"`
 }
+
+type PaymentActivity struct {
+	Id               int64     `json:"id"`
+	Customer         Customer  `json:"customer"`
+	TargetId         int64     `json:"target_id"`
+	Items            []Item    `json:"items"`
+	Amount           int       `json:"amount"`
+	OrderNumber      string    `json:"order_number"`
+	OrderDescription string    `json:"order_description"`
+	CreatedAt        time.Time `json:"created_at"`
+}
