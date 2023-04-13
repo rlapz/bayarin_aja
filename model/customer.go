3dd0a8801eaeffab0e16790d7bcf0a9ba773af37
Arthur Lapz <rlapz@gnuweeb.org>
2023-04-14 01:29:31 +0700

    model: customer.go: Add customer activity model
    This is the `history` of the required interview assignment. (for the
    customer only)
    I'd like to use the word `activity` instead.

--- model/customer.go
+++ model/customer.go
@@ -12,3 +12,11 @@ type Customer struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
+type CustomerActivity struct {
+	Id         int64 `json:"id"`
+	CustomerId int64 `json:"customer_id"`
+	// What activity a customer did? Like: login, logout, .etc
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+}
