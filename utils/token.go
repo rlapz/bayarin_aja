a1488923bfa42c5150ae6a824476e0c1cc04e905
Arthur Lapz <rlapz@gnuweeb.org>
2023-04-14 02:50:42 +0700

    utils: token: Add `TokenValidate` function
    This function is to be used for the middleware

--- utils/token.go
+++ utils/token.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt"
@@ -25,4 +26,24 @@ func newClaims(id int64, exp time.Duration) jwt.MapClaims {
 	return jwt.MapClaims{
 		"id": id,
 	}
+}
+
+func TokenValidate(token string, secret []byte) (int64, error) {
+	tok, err := jwt.Parse(token, func(t *jwt.Token) (any, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("a given token cannot be parsed")
+		}
+
+		return secret, nil
+	})
+
+	if err != nil {
+		return -1, err
+	}
+
+	if cl, ok := tok.Claims.(jwt.MapClaims); ok && tok.Valid {
+		return int64(cl["id"].(float64)), nil
+	}
+
+	return -1, errors.New("invalid token")
 }
